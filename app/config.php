bff5fa9fa759921aada5889555b7651c634ff0c7
LinneaEriksson <Linnea-E@hotmail.com>
2021-12-15 10:45:03 +0100

    changed title

--- app/config.php
+++ app/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 // This file contains a list of global configuration settings.
 
 return [
-    'title' => 'Project Name',
-    'database_path' => sprintf('sqlite:%s/database/database.db', __DIR__),
+    'title' => 'ToDo',
+    'database_path' => sprintf('sqlite:%s/database/datalist.db', __DIR__),
 ];
