39e1e928831ae418b2bb39ea179ce50f5884fce0
LinneaEriksson <Linnea-E@hotmail.com>
2021-12-15 12:46:31 +0100

    test upload picture-function

--- app/users/register.php
+++ app/users/register.php
@@ -6,4 +6,4 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
-redirect('/');
+// redirect('/');
--- signup.php
+++ signup.php
@@ -1,41 +1,71 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
-<?php require __DIR__ . '/views/header.php'; ?>
+<?php require __DIR__ . '/views/header.php';
+
+$date = date("y-m-d");
+$errors = [];
+
+if (isset($_FILES['avatar'])) {
+  $avatar = $_FILES['avatar'];
+  $destination =  __DIR__ . '/app/database/userimages/' . $date . $avatar['name'];
+
+
+  if (!in_array($avatar['type'], ['image/jpeg', 'image/png'])) {
+    $errors[] = "The image file type is not allowed.";
+  }
+
+  if ($avatar['size'] > 2097152) {
+    $errors[] = "The uploaded file exceeded the file size limit.";
+  }
+
+  if (count($errors) === 0) {
+    move_uploaded_file($avatar['tmp_name'], $destination);
+    $message = 'The file was successfully uploaded!';
+  }
+}
+?>
 
 <h1>Sign up here!</h1>
 
-<form action="app/users/login.php" method="post">
+<form action="signup.php" method="post" enctype="multipart/form-data">
+  <!-- Form for username -->
   <div class="mb-3">
     <label for="username">Username</label>
     <input class="form-control" type="username" name="username" id="username" placeholder="Write your username" required>
     <small class="form-text">Create your username</small>
   </div>
+  <!-- form for email -->
+  <div class="mb-3">
+    <label for="email">Email</label>
+    <input class="form-control" type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
+    <small class="form-text">Please provide the your email address.</small>
+  </div>
 
-  <form action="app/users/register.php" method="post">
-    <div class="mb-3">
-      <label for="email">Email</label>
-      <input class="form-control" type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
-      <small class="form-text">Please provide the your email address.</small>
-    </div>
+  <!-- form for password -->
+  <div class="mb-3">
+    <label for="password">Password</label>
+    <input class="form-control" type="password" name="password" id="password" placeholder="Please create a password" required>
+    <small class="form-text">Create your password</small>
+  </div>
+  <!-- form for profilepicture -->
+  <div class="mb-3">
+    <label for="avatar">Choose a PNG image to upload</label>
+    <input type="file" name="avatar" id="avatar" accept=".png" required>
+  </div>
 
-    <form action="app/users/login.php" method="post">
-      <div class="mb-3">
-        <label for="password">Password</label>
-        <input class="form-control" type="password" name="password" id="password" placeholder="Please create a password" required>
-        <small class="form-text">Create your password</small>
-      </div>
+  <button type="submit" class="btn btn-primary">Sign up!</button>
+</form>
 
-      <div class="mb-3">
-        <label for="profileImage">Profile picture</label>
-        <input class="form-control" type="profileImage" name="profileImage" id="profileImage" required>
-        <small class="form-text">Please provide the your profile picture</small>
-      </div>
+<?php foreach ($errors as $error) : ?>
+  <p class="error"><?php echo $error; ?></p>
+<?php endforeach; ?>
 
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+<?php if (isset($message)) : ?>
+  <p class="success"><?php echo $message; ?></p>
+<?php endif; ?>
 
-    <!-- class = form-control är bootstrap och alltså en förinställd css. DU KAN ÄNDRA DET HÄR!!!  -->
-    <!-- primary i btn-primary är förinställd färg. Det finns olika färger, googla "bootstrapcolor" så kommer det upp!  -->
+<!-- class = form-control är bootstrap och alltså en förinställd css. DU KAN ÄNDRA DET HÄR!!!  -->
+<!-- primary i btn-primary är förinställd färg. Det finns olika färger, googla "bootstrapcolor" så kommer det upp!  -->
 
 
 
-    <?php require __DIR__ . '/views/footer.php'; ?>
+<?php require __DIR__ . '/views/footer.php'; ?>
