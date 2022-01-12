7cbb0a4a1642a3c25ccbe8a6e2e15f6de6b61ebb
LinneaEriksson <Linnea-E@hotmail.com>
2022-01-12 21:07:36 +0100

    fixed error

--- views/navigation.php
+++ views/navigation.php
@@ -6,17 +6,13 @@
             <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
         </li>
 
-
         <?php if (isset($_SESSION['user']) === true) { ?>
-
             <li class="nav-item">
                 <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/tasks.php' ? 'active' : ''; ?>" href="/tasks.php">Tasks</a>
             </li>
-
             <li class="nav-item">
                 <a class="nav-link" href="editProfile.php">Edit profile</a>
             </li>
-
         <?php } ?>
 
         <li class="nav-item">
@@ -26,7 +22,5 @@
                 <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
             <?php endif; ?>
         </li>
-
-
     </ul>
 </nav>
